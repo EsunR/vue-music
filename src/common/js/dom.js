37d9081139f24c18fcf7de0d2faf0f3dd950e2df
EsunR <641411169@qq.com>
2019-07-01 17:16:30 +0800

    music-list组件开发完成 第七章

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -42,7 +42,28 @@ export function getSetData(el, name, val) {
 
 // 能力检测
 let elementStyle = document.createElement('div').style
-
 let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key;
+    }
+  }
+  return false;
+})()
 
-})
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false;
+  }
+  if (vendor === 'standard') {
+    return style;
+  }
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length > 0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -19,27 +25,30 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import Scroll from "base/scroll/scroll";
-// import Loading from "base/loading/loading";
+import Loading from "base/loading/loading";
 import SongList from "base/song-list/song-list";
-// import {prefixStyle} from 'common/js/dom'
+import { prefixStyle } from "common/js/dom";
 // import {playlistMixin} from 'common/js/mixin'
 // import {mapActions} from 'vuex'
 
 const RESERVED_HEIGHT = 40;
-// const transform = prefixStyle('transform')
-// const backdrop = prefixStyle('backdrop-filter')
+const transform = prefixStyle("transform");
+const backdrop = prefixStyle("backdrop-filter");
 
 export default {
   components: {
     Scroll,
-    SongList
-    // Loading
+    SongList,
+    Loading
   },
   data() {
     return {
@@ -70,6 +79,9 @@ export default {
   methods: {
     scroll(pos) {
       this.scrollY = pos.y;
+    },
+    back() {
+      this.$router.back();
     }
   },
   watch: {
@@ -94,20 +106,20 @@ export default {
       } else {
         blur = Math.min(20 * percent, 20);
       }
-      this.$refs.filter.style["backdrop-filter"] = `blur(${blur}px)`;
-      this.$refs.filter.style["webkitBackdrop-filter"] = `blur(${blur}px)`;
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`;
       // 当列表滑动到顶部时，改变图片的高度以及index，让背景图图层上最顶部遮挡文字
       if (newY < this.minTranslateY) {
         zIndex = 10;
         this.$refs.bgImage.style.paddingTop = 0;
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+        this.$refs.playBtn.style.display = "none";
       } else {
         this.$refs.bgImage.style.paddingTop = "70%";
         this.$refs.bgImage.style.height = 0;
+        this.$refs.playBtn.style.display = "";
       }
       this.$refs.bgImage.style.zIndex = zIndex;
-      this.$refs.bgImage.style["transform"] = `scale(${scale})`;
-      this.$refs.bgImage.style["webkitTransform"] = `scale(${scale})`;
+      this.$refs.bgImage.style[transform] = `scale(${scale})`;
     }
   },
   created() {
